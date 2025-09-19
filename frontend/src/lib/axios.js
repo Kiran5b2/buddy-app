f6adfd9206a44bb881dc92caacb98c17f90f4b0a
L N Surya Kiran <lsuryakiran2004@.com>
2025-09-19 13:03:55 +0530

    reverse commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://buddy-app-1.onrender.com/api', // Updated to deployed backend
+  baseURL: 'http://localhost:5001/api', // Replace with your backend API base URL
   withCredentials: true, // Include cookies in requests
 });
