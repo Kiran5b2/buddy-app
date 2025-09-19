aa55cbb5a4431edadbdcbbca2a808c7512f54bbc
L N Surya Kiran <lsuryakiran2004@.com>
2025-09-19 12:36:43 +0530

    committing

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -6,7 +6,6 @@ dotenv.config();
 
 export const connectDB = async () => {
     try {
-        console.log(process.env.MONGO_URI);
         await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connected");
     } catch (error) {
